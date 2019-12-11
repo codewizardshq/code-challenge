c61a0cac4fff649cba60d2accd885b6f3dd6a9a0
Samuel Hoffman <samuelhoffman2@gmail.com>
2019-12-11 18:12:44 -0500

    suppress limiter extenion warning

--- CodeChallenge/limiter.py
+++ CodeChallenge/limiter.py
@@ -1,7 +1,8 @@
 from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask_jwt_extended import get_current_user
 
-limiter = Limiter()
+limiter = Limiter(key_func=get_remote_address)
 
 
 def user_rank():
