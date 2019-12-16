da4438f2f6d566c7e17277c7b6bbe7c3b547061c
Samuel Hoffman <samuelhoffman2@gmail.com>
2019-12-16 12:34:59 -0500

    models: cascade deletes on answers

--- CodeChallenge/models.py
+++ CodeChallenge/models.py
@@ -26,8 +26,9 @@ class Answer(db.Model):
     """Tracks a user answering a question"""
     id = db.Column(db.Integer, primary_key=True)
     question_id = db.Column(db.Integer,
-                            db.ForeignKey("question.id"),
+                            db.ForeignKey("question.id", ondelete="cascade"),
                             nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey("users.id", ondelete="cascade"))
     text = db.Column(db.String(2000))
     correct = db.Column(db.Boolean)
