ce3d84e2b51073ce0e83fc1dde7aca6a4739038f
net8floz <net8floz@gmail.com>
2020-01-03 11:34:18 -0800

    Linter fixes

--- src/views/Register/Step2.vue
+++ src/views/Register/Step2.vue
@@ -1,90 +1,99 @@
-<template></template>
-
 <template>
-	<v-form @submit.prevent="validate" ref="form" v-model="isValid">
-		<v-card-text>
-			<v-row no-gutters>
-				<v-col>
-					<v-text-field
-						v-bind="fields.firstName"
-						v-model="fields.firstName.value"
-						:disabled="isSubmitting"
-					/>
-				</v-col>
-				<v-col>
-					<v-text-field
-						v-bind="fields.lastName"
-						v-model="fields.lastName.value"
-						:disabled="isSubmitting"
-					/>
-				</v-col>
-			</v-row>
-			<v-text-field
-				v-bind="fields.studentEmail"
-				v-model="fields.studentEmail.value"
-				:disabled="isSubmitting"
-			/>
-			<v-menu
-				v-model="showCalendar"
-				:close-on-content-click="false"
-				:nudge-right="40"
-				transition="scale-transition"
-				offset-y
-				min-width="290px"
-			>
-				<template v-slot:activator="{ on }">
-					<v-text-field
-						v-model="fields.dateOfBirth.value"
-						prepend-icon="mdi-calendar"
-						readonly
-						v-on="on"
-						v-bind="fields.dateOfBirth"
-					></v-text-field>
-				</template>
-				<v-date-picker v-model="fields.dateOfBirth.value" @input="showCalendar = false" />
-			</v-menu>
-		</v-card-text>
+  <v-form @submit.prevent="validate" ref="form" v-model="isValid">
+    <v-card-text>
+      <v-row no-gutters>
+        <v-col>
+          <v-text-field
+            v-bind="fields.firstName"
+            v-model="fields.firstName.value"
+            :disabled="isSubmitting"
+          />
+        </v-col>
+        <v-col>
+          <v-text-field
+            v-bind="fields.lastName"
+            v-model="fields.lastName.value"
+            :disabled="isSubmitting"
+          />
+        </v-col>
+      </v-row>
+      <v-text-field
+        v-bind="fields.studentEmail"
+        v-model="fields.studentEmail.value"
+        :disabled="isSubmitting"
+      />
+      <v-menu
+        v-model="showCalendar"
+        :close-on-content-click="false"
+        :nudge-right="40"
+        transition="scale-transition"
+        offset-y
+        min-width="290px"
+      >
+        <template v-slot:activator="{ on }">
+          <v-text-field
+            v-model="fields.dateOfBirth.value"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-on="on"
+            v-bind="fields.dateOfBirth"
+          ></v-text-field>
+        </template>
+        <v-date-picker
+          v-model="fields.dateOfBirth.value"
+          @input="showCalendar = false"
+        />
+      </v-menu>
+    </v-card-text>
 
-		<v-card-actions>
-			<v-btn color="primary" @click="() => $emit('back')" :disabled="isSubmitting">Back</v-btn>
-			<v-spacer />
-			<v-btn color="primary" type="submit" :disabled="isSubmitting">
-				Next
-				<v-progress-circular size="14" class="ml-3" indeterminate v-if="isSubmitting" />
-			</v-btn>
-		</v-card-actions>
-	</v-form>
+    <v-card-actions>
+      <v-btn
+        color="primary"
+        @click="() => $emit('back')"
+        :disabled="isSubmitting"
+        >Back</v-btn
+      >
+      <v-spacer />
+      <v-btn color="primary" type="submit" :disabled="isSubmitting">
+        Next
+        <v-progress-circular
+          size="14"
+          class="ml-3"
+          indeterminate
+          v-if="isSubmitting"
+        />
+      </v-btn>
+    </v-card-actions>
+  </v-form>
 </template>
 
 <script>
-import { auth } from "@/api";
-
 export default {
-	name: "register-step-2",
-	props: ["fields"],
-	methods: {
-		async submit() {
-			if (this.isSubmitting) {
-				return;
-			}
-			this.isSubmitting = true;
-			const cb = () => {
-				this.isSubmitting = false;
-			};
-			this.$emit("submit", cb);
-		},
-		validate() {
-			if (this.$refs.form.validate()) {
-				this.submit();
-			}
-		}
-	},
-	data() {
-		return {
-			isValid: false,
-			isSubmitting: false,
-			showCalendar: false
-		};
-	}
+  name: "register-step-2",
+  props: ["fields"],
+  methods: {
+    async submit() {
+      if (this.isSubmitting) {
+        return;
+      }
+      this.isSubmitting = true;
+      const cb = () => {
+        this.isSubmitting = false;
+      };
+      this.$emit("submit", cb);
+    },
+    validate() {
+      if (this.$refs.form.validate()) {
+        this.submit();
+      }
+    }
+  },
+  data() {
+    return {
+      isValid: false,
+      isSubmitting: false,
+      showCalendar: false
+    };
+  }
 };
 </script>
--- src/views/Register/index.vue
+++ src/views/Register/index.vue
@@ -1,40 +1,44 @@
 <template>
-	<v-row align="center" justify="center">
-		<v-col cols="12" sm="8" md="8">
-			<v-card flat class="mt-12">
-				<v-toolbar color="primary" dark flat>
-					<v-toolbar-title>Create Your Account</v-toolbar-title>
-				</v-toolbar>
-				<v-stepper v-model="e1">
-					<v-stepper-header class="elevation-0">
-						<v-stepper-step :complete="e1 > 1" step="1">Account Details</v-stepper-step>
+  <v-row align="center" justify="center">
+    <v-col cols="12" sm="8" md="8">
+      <v-card flat class="mt-12">
+        <v-toolbar color="primary" dark flat>
+          <v-toolbar-title>Create Your Account</v-toolbar-title>
+        </v-toolbar>
+        <v-stepper v-model="e1">
+          <v-stepper-header class="elevation-0">
+            <v-stepper-step :complete="e1 > 1" step="1"
+              >Account Details</v-stepper-step
+            >
 
-						<v-divider></v-divider>
+            <v-divider></v-divider>
 
-						<v-stepper-step :complete="e1 > 2" step="2">Student Details</v-stepper-step>
+            <v-stepper-step :complete="e1 > 2" step="2"
+              >Student Details</v-stepper-step
+            >
 
-						<v-divider></v-divider>
+            <v-divider></v-divider>
 
-						<v-stepper-step step="3">Terms Of Use</v-stepper-step>
-					</v-stepper-header>
+            <v-stepper-step step="3">Terms Of Use</v-stepper-step>
+          </v-stepper-header>
 
-					<v-stepper-items>
-						<v-stepper-content step="1">
-							<step-1 :fields="fields" @submit="submit1" />
-						</v-stepper-content>
+          <v-stepper-items>
+            <v-stepper-content step="1">
+              <step-1 :fields="fields" @submit="submit1" />
+            </v-stepper-content>
 
-						<v-stepper-content step="2">
-							<step-2 :fields="fields" @back="back" @submit="submit2" />
-						</v-stepper-content>
+            <v-stepper-content step="2">
+              <step-2 :fields="fields" @back="back" @submit="submit2" />
+            </v-stepper-content>
 
-						<v-stepper-content step="3">
-							<step-3 :fields="fields" @back="back" @submit="submit3" />
-						</v-stepper-content>
-					</v-stepper-items>
-				</v-stepper>
-			</v-card>
-		</v-col>
-	</v-row>
+            <v-stepper-content step="3">
+              <step-3 :fields="fields" @back="back" @submit="submit3" />
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-stepper>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -44,108 +48,109 @@ import Step3 from "./Step3";
 import { auth } from "@/api";
 
 export default {
-	components: {
-		Step1,
-		Step2,
-		Step3
-	},
-	methods: {
-		back() {
-			this.e1--;
-			if (this.e1 <= 0) {
-				this.e1 = 1;
-			}
-		},
-		submit1(cb) {
-			this.e1++;
-			cb();
-		},
-		submit2(cb) {
-			this.e1++;
-			cb();
-		},
-		async submit3(cb) {
-			if (this.isSubmitting) {
-				return;
-			}
-			this.isSubmitting = true;
-			try {
-				await auth.createAccount(
-					this.fields.parentEmail.value,
-					this.fields.password.value,
-					this.fields.firstName.value,
-					this.fields.lastName.value
-				);
-				localStorage.setItem("lastEmail", this.fields.parentEmail.value);
-				this.$store.dispatch("Snackbar/showInfo", "Successfully Logged In");
-				this.$router.push({ name: "home" });
-			} catch (err) {
-				this.$store.dispatch("Snackbar/showError", err);
-			}
-			this.isSubmitting = false;
-			this.e1 = 0;
-			this.$router.push({ name: "quiz" });
-		}
-	},
-	data() {
-		return {
-			isSubmitting: false,
-			e1: 0,
-			fields: {
-				parentEmail: {
-					label: "Parent's E-mail Address",
-					type: "email",
-					value: "",
-					rules: [v => !!v || "Please provide a email"]
-				},
-				studentEmail: {
-					label: "Student's E-mail Address (optional)",
-					type: "email",
-					value: ""
-				},
-				password: {
-					label: "Password",
-					type: "password",
-					value: "",
-					rules: [
-						v => !!v || "Don't forget to give a password",
-						v => v.length >= 11 || "Password must be atleast 11 characters"
-					]
-				},
-				passwordConfirm: {
-					label: "Confirm Password",
-					type: "password",
-					value: "",
-					rules: [
-						v => v == this.fields.password.value || "Passwords do not match"
-					]
-				},
-				firstName: {
-					label: "Student's First Name",
-					type: "text",
-					value: "",
-					rules: [v => !!v || "Please tells us your name"]
-				},
-				lastName: {
-					label: "Student's Last Name",
-					type: "text",
-					value: "",
-					rules: [v => !!v || "Please tells us your name"]
-				},
-				dateOfBirth: {
-					label: "Student's Date Of Birth",
-					type: "date",
-					value: null,
-					max: new Date().toISOString().substr(0, 10),
-					rules: [v => !!v || "Please enter a date of birth"]
-				},
-				tos: {
-					label: "I agree to the Terms of Use",
-					value: false,
-					rules: [v => !!v || "You must agree to the Terms of Use"]
-				}
-			}
-		};
-	}
+  components: {
+    Step1,
+    Step2,
+    Step3
+  },
+  methods: {
+    back() {
+      this.e1--;
+      if (this.e1 <= 0) {
+        this.e1 = 1;
+      }
+    },
+    submit1(cb) {
+      this.e1++;
+      cb();
+    },
+    submit2(cb) {
+      this.e1++;
+      cb();
+    },
+    async submit3(cb) {
+      if (this.isSubmitting) {
+        return;
+      }
+      this.isSubmitting = true;
+      try {
+        await auth.createAccount(
+          this.fields.parentEmail.value,
+          this.fields.password.value,
+          this.fields.firstName.value,
+          this.fields.lastName.value
+        );
+        localStorage.setItem("lastEmail", this.fields.parentEmail.value);
+        this.$store.dispatch("Snackbar/showInfo", "Successfully Logged In");
+        this.$router.push({ name: "home" });
+      } catch (err) {
+        this.$store.dispatch("Snackbar/showError", err);
+      }
+      this.isSubmitting = false;
+      this.e1 = 0;
+      this.$router.push({ name: "quiz" });
+      cb();
+    }
+  },
+  data() {
+    return {
+      isSubmitting: false,
+      e1: 0,
+      fields: {
+        parentEmail: {
+          label: "Parent's E-mail Address",
+          type: "email",
+          value: "",
+          rules: [v => !!v || "Please provide a email"]
+        },
+        studentEmail: {
+          label: "Student's E-mail Address (optional)",
+          type: "email",
+          value: ""
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          value: "",
+          rules: [
+            v => !!v || "Don't forget to give a password",
+            v => v.length >= 11 || "Password must be atleast 11 characters"
+          ]
+        },
+        passwordConfirm: {
+          label: "Confirm Password",
+          type: "password",
+          value: "",
+          rules: [
+            v => v == this.fields.password.value || "Passwords do not match"
+          ]
+        },
+        firstName: {
+          label: "Student's First Name",
+          type: "text",
+          value: "",
+          rules: [v => !!v || "Please tells us your name"]
+        },
+        lastName: {
+          label: "Student's Last Name",
+          type: "text",
+          value: "",
+          rules: [v => !!v || "Please tells us your name"]
+        },
+        dateOfBirth: {
+          label: "Student's Date Of Birth",
+          type: "date",
+          value: null,
+          max: new Date().toISOString().substr(0, 10),
+          rules: [v => !!v || "Please enter a date of birth"]
+        },
+        tos: {
+          label: "I agree to the Terms of Use",
+          value: false,
+          rules: [v => !!v || "You must agree to the Terms of Use"]
+        }
+      }
+    };
+  }
 };
 </script>
