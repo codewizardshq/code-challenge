052cd6de6a08b54d2dcb8430f186d19f71e4b9a1
Samuel Hoffman <samuelhoffman2@gmail.com>
2020-01-21 21:43:12 -0500

    remove key from conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,7 +18,6 @@ def client(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
     app.config["MAIL_SUPPRESS_SEND"] = True
     app.config["CODE_CHALLENGE_START"] = time.time()
-    app.config["MG_PRIVATE_KEY"] = 'key-e49d7f79a91021260574182a48a482e8'
 
     with app.test_client() as client:
         with app.app_context():
