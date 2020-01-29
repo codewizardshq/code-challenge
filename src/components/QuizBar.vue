3b5addb66f7620379027a1ad4bb69311c1988525
net8floz <net8floz@gmail.com>
2020-01-29 10:45:58 -0800

    app mode tweaks

--- src/components/QuizBar.vue
+++ src/components/QuizBar.vue
@@ -1,13 +1,16 @@
 <template>
 	<v-toolbar color="dark2 quiz-bar" flat class="secondary--text" :height="60" :max-height="60">
-		<div class="quiz-bar-rank" v-show="User.isAuthorized">
+		<div class="quiz-bar-rank" v-show="User.isAuthorized && Quiz.quizHasStarted && !Quiz.quizHasEnded">
 			<div class="level-display">Level</div>
 			<div class="rank">{{ User.rank }}</div>
 		</div>
 		<v-container>
 			<v-row>
 				<v-col>
-					<span v-if="User.isAuthorized" class="barrow-bold">Welcome pilgrim {{ User.displayName }}</span>
+					<span
+						v-if="User.isAuthorized"
+						class="barrow-bold"
+					>Welcome pilgrim {{ User.displayName }}</span>
 
 					<router-link
 						v-else
