72a60b409072a6fd956985457a4f548de4e26ebd
Samuel Hoffman <samuelhoffman2@gmail.com>
2020-01-29 18:00:23 -0500

    scaffolding for daily email

--- CodeChallenge/api/eb.py
+++ CodeChallenge/api/eb.py
@@ -1,4 +1,10 @@
-from flask import Blueprint
+from hmac import compare_digest
+
+from flask import Blueprint, request, current_app, render_template
+from flask_mail import Message
+
+from .. import core
+from ..mail import mail
 
 bp = Blueprint("awsebapi", __name__, url_prefix="/api/v1/eb")
 
@@ -7,3 +13,30 @@ bp = Blueprint("awsebapi", __name__, url_prefix="/api/v1/eb")
 @bp.route("/health", methods=["GET"])
 def eb_health_check():
     return "OK", 200
+
+
+# POST request from an AWS Lambda function once per day
+# any daily tasks should be placed here
+@bp.route("/worker", methods=["POST"])
+def worker():
+    try:
+        password = request.json["password"]
+    except (TypeError, KeyError):
+        return "", 400
+
+    if not compare_digest(password,
+                          current_app.config["WORKER_PASSWORD"]):
+        return "", 401
+
+    # send daily reminder emails only while challenge is active
+
+    if core.day_number() >= 1 and not core.challenge_ended():
+        msg = Message("CHANGEME",
+                      sender=current_app.config["MAIL_DEFAULT_SENDER"],
+                      recipients=[current_app.config["MG_LIST"]])
+
+        msg.html = render_template("CHANGEME")
+
+        mail.send(msg)
+
+    return "", 200
