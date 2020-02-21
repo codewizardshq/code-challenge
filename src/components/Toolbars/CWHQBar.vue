e414718ad389c2c468687b1258b59fcb1b4c4c31
Kyle Askew <net8floz@gmail.com>
2020-02-20 16:28:17 -0800

    Update CWHQBar.vue

--- src/components/Toolbars/CWHQBar.vue
+++ src/components/Toolbars/CWHQBar.vue
@@ -1,9 +1,6 @@
 <template>
   <v-toolbar class="cwhq-bar" :height="height" :max-height="height">
     <v-container>
-      <a href="https://github.com/codewizardshq/code-challenge"
-        >See the source on Github</a
-      >
       <a href="https://codewizardshq.com/challenge">Back to CodeWizardsHQ</a>
     </v-container>
   </v-toolbar>
