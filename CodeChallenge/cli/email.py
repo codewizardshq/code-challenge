ead739bb28f9186da3c1dc76dece7b3a4d19d63f
Samuel Hoffman <samuelhoffman2@gmail.com>
2020-02-27 16:47:31 -0500

    emailcli: remove print() and fix CLI colors

--- CodeChallenge/cli/email.py
+++ CodeChallenge/cli/email.py
@@ -12,7 +12,6 @@ bp = Blueprint("emailcli", __name__, cli_group="email")
 
 def get_template_variables(filename):
     path = os.path.join(current_app.root_path, "templates")
-    print(path)
     env = Environment(loader=FileSystemLoader(path))
     template_source = env.loader.get_source(env, filename)[0]
     parsed_content = env.parse(template_source)
@@ -35,11 +34,11 @@ def email_send(to, subject, template):
         try:
             filled_vars[var_name] = input(f"value for {var_name!r}: ")
         except KeyboardInterrupt:
-            click.secho("sending canceled", color="yellow")
+            click.secho("\nsending canceled", fg="yellow")
             return
 
     msg.html = render_template(template, **filled_vars)
 
     mail.send(msg)
 
-    click.secho("message send", color="green")
+    click.secho("message sent", fg="green")
