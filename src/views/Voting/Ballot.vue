fdac7ad83017c227109cbacc1e1675059a34b07a
net8floz <net8floz@gmail.com>
2020-03-18 11:20:12 -0700

    begins search frontend code

--- src/views/Voting/Ballot.vue
+++ src/views/Voting/Ballot.vue
@@ -4,30 +4,20 @@
       <v-col>
         <h2 class="ballot-header">
           Cast your vote below!
-        </h2></v-col
-      >
+        </h2>
+        <search-bar />
+      </v-col>
     </v-row>
     <v-row justify="center" v-if="isLoading">
       <v-col class="text-center">
-        <v-progress-circular
-          class="mt-6"
-          color="cwhqBlue"
-          size="100"
-          width="10"
-          indeterminate
-        />
+        <v-progress-circular class="mt-6" color="cwhqBlue" size="100" width="10" indeterminate />
         <h2 class="mt-6">
           Loading Results <small><br />Please Wait</small>
         </h2>
       </v-col>
     </v-row>
     <v-row justify="center" v-else>
-      <ballot-card
-        v-for="(item, i) in items"
-        :key="i"
-        v-bind="item"
-        @click="showCode(item)"
-      />
+      <ballot-card v-for="(item, i) in items" :key="i" v-bind="item" @click="showCode(item)" />
     </v-row>
     <v-row justify="center" v-if="totalPages > 1">
       <v-pagination v-model="page" :length="totalPages" circle></v-pagination>
@@ -37,15 +27,17 @@
 </template>
 
 <script>
-import Vue from "vue";
-import { voting } from "@/api";
-import BallotCard from "./BallotCard";
-import CodeModal from "./CodeModal";
+import Vue from 'vue';
+import { voting } from '@/api';
+import BallotCard from './BallotCard';
+import CodeModal from './CodeModal';
+import SearchBar from './SearchBar';
 
 export default {
   components: {
     BallotCard,
-    CodeModal
+    CodeModal,
+    SearchBar
   },
   data() {
     return {
@@ -77,7 +69,7 @@ export default {
           Vue.set(this, key, value);
         }
       } catch (err) {
-        this.$router.push({ name: "redirect" });
+        this.$router.push({ name: 'redirect' });
       }
       this.isLoading = false;
     }
@@ -97,7 +89,7 @@ export default {
 h2 {
   text-align: center;
   color: #0d1d41;
-  font-family: "Barlow", sans-serif;
+  font-family: 'Barlow', sans-serif;
   font-weight: bold;
   margin-bottom: 12px;
 }
---
+++ src/views/Voting/SearchBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-text-field outlined light prepend-inner-icon="mdi-magnify" placeholder="Search by name" v-model="text"></v-text-field>
+  </div>
+</template>
+
+<script>
+import * as api from '@/api';
+export default {
+  data() {
+    return {
+      text: ''
+    };
+  },
+  watch: {
+    text(text) {
+      api.voting.search(text);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
