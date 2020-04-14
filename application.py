0b3169c0fb17090d0a56eee4117b10338b4a5de0
Sam <sam@codewizardshq.com>
2020-04-14 16:59:37 -0400

    support FLASK_ENV='testing'

--- application.py
+++ application.py
@@ -6,10 +6,12 @@ import os
 
 from CodeChallenge import create_app
 
-FLASK_ENV = os.environ.get("FLASK_ENV")
+FLASK_ENV = os.getenv("FLASK_ENV")
 
-if FLASK_ENV and FLASK_ENV == "development":
+if FLASK_ENV == "development":
     cfg = "DevelopmentConfig"
+elif FLASK_ENV == "testing":
+    cfg = "TestingConfig"
 else:
     cfg = "ProductionConfig"
 
