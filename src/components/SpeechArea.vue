a61a54ee9a0dfdcfce4c381e4a44b4eff24279cc
Sam <samuelhoffman2@gmail.com>
2021-03-23 14:35:07 -0400

    frontend: reskin question screen

--- src/components/SpeechArea.vue
+++ src/components/SpeechArea.vue
@@ -1,26 +1,59 @@
 <template>
   <div class="speech-area">
     <div class="speech-graphic"></div>
+
     <div class="speech-head">
       <div class="speech-title">
         <slot name="title" />
       </div>
     </div>
+
     <div class="speech-body">
       <slot name="default" />
+    </div>
+
+    <div class="answer-input">
+      <input
+        type="text"
+        id="input-text-field"
+        v-if="answerInputType === 'text-field'"
+      />
+      <textarea v-else></textarea>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "SpeechArea"
+  name: "SpeechArea",
+  props: {
+    answerInputType: {
+      type: String,
+      default: "text-field"
+    }
+  }
 };
 </script>
 
 <style lang="sass" scoped>
+#input-text-field
+  position: relative
+  z-index: 2
+  color: white
+  width: 100%
+  height: 100%
+
 .speech-area
   position: relative
+
+  .answer-input
+    z-index: 1
+    position: absolute
+    width: 334px
+    height: 75px
+    right: -135px
+    top: 290px
+
 
   .speech-head
     font-size: 30px
@@ -32,19 +65,24 @@ export default {
     .speech-title
       text-align: center
       position: relative
+      left: -287px
+      top: -8px
 
   .speech-body
     z-index: 1
     position: relative
-    text-align: center
-    margin-top: 100px
+    text-align: left
+    width: 560px
+    left: -287px
+    top: 55px
+    height: 495px
 
   .speech-graphic
     position: absolute
     z-index: 0
-    width: 666px
-    height: 2238px
-    left: -166px
-    background-image: url("/images/main-text-area.svg")
+    width: 1200px
+    height: 666px
+    left: -333px
+    background-image: url("/images/main-text-and-input-areas.svg")
     background-size: contain
 </style>
--- src/views/Quiz/Quiz.vue
+++ src/views/Quiz/Quiz.vue
@@ -1,30 +1,31 @@
 <template>
   <v-container fluid>
     <v-row justify="center">
-      <quiz-scroll>
-        <template v-slot:title>Level {{ rank }}</template>
+      <speech-area>
+        <template v-slot:title>The Tale of Two Suns</template>
         <template v-slot:default>
-          <img class="asset" :src="'/' + asset" v-if="!!asset" />
-          <div class="scroll-content" v-html="question" />
+          <!--
+          <img class="asset" :src="'/' + asset" v-if="!!asset" /> -->
+          <div v-html="question" />
         </template>
-      </quiz-scroll>
+      </speech-area>
+      <!--
       <quiz-answer :rank="rank" @next="onNext" />
+      -->
     </v-row>
     <quiz-need-help />
   </v-container>
 </template>
 
 <script>
-import QuizScroll from "@/components/QuizScroll";
-import QuizAnswer from "./QuizAnswer.vue";
 import QuizNeedHelp from "@/components/QuizNeedHelp";
 import { User, Quiz } from "@/store";
+import SpeechArea from "@/components/SpeechArea";
 
 export default {
   name: "quiz",
   components: {
-    QuizScroll,
-    QuizAnswer,
+    SpeechArea,
     QuizNeedHelp
   },
   data() {
@@ -40,8 +41,8 @@ export default {
     this.asset = this.Quiz.asset;
   },
   methods: {
-    async onNext() {
-      this.$router.go();
+    async onNext(n) {
+      this.$router.go(n);
     }
   },
   computed: {
