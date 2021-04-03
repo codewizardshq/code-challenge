f38f636880945a2b1b5d56f88136f3e7d038149c
Sam <samuelhoffman2@gmail.com>
2021-04-02 23:41:40 -0400

    remove unneeded div

--- src/views/Quiz/QuizIntro.vue
+++ src/views/Quiz/QuizIntro.vue
@@ -1,29 +1,27 @@
 <template>
   <div class="mt-6">
     <v-row justify="center">
-      <div class="container">
-        <speech-area>
-          <template v-slot:title>Welcome</template>
-          <template v-slot:default>
-            <p class="text-center">Watch the video</p>
-            <p class="text-center">
-              <iframe
-                width="500px"
-                height="360px"
-                src="https://www.youtube.com/embed/G1LcHvSJL8M"
-                frameborder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen
-              ></iframe>
-            </p>
-            <p class="text-center">
-              <v-btn color="secondary darken-2" @click="onClick" x-large
-                >OK!
-              </v-btn>
-            </p>
-          </template>
-        </speech-area>
-      </div>
+      <speech-area>
+        <template v-slot:title>Welcome</template>
+        <template v-slot:default>
+          <p class="text-center">Watch the video</p>
+          <p class="text-center">
+            <iframe
+              width="500px"
+              height="360px"
+              src="https://www.youtube.com/embed/G1LcHvSJL8M"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </p>
+          <p class="text-center">
+            <v-btn color="secondary darken-2" @click="onClick" x-large
+              >OK!
+            </v-btn>
+          </p>
+        </template>
+      </speech-area>
     </v-row>
   </div>
 </template>
