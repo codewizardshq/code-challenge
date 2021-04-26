641fc2a4031288f8b04840b5c2d330fbb3f6b755
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2021-04-26 14:29:41 -0700

    add image for Ballot, update text, fix styling in Voting/App, VoteWoah, and VotingOver

--- src/views/Voting/VoteWoah.vue
+++ src/views/Voting/VoteWoah.vue
@@ -1,34 +1,27 @@
 <template>
-  <div class="vote-woah-container">
-    <v-container>
-      <v-row>
-        <v-col class="inside-div">
-          <h2 class="ballot-header">
-            Woah, voting has not started yet!
-          </h2>
-          <p>
-            Our challengers are hard at work. Come back on May 3 to vote for the
-            winner.
-          </p>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col class="inside-div">
+        <h2 class="ballot-header">
+          Woah, voting has not started yet!
+        </h2>
+        <p>
+          Our challengers are hard at work. Come back on May 3 to vote for the
+          winner.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+// TODO: this and VotingOver should be a single component with props
 export default {
   name: "VotingWoah"
 };
 </script>
 
 <style lang="scss" scoped>
-.vote-woah-container {
-  height: 100%;
-  width: 100%;
-  background: url("/images/space-background.jpg");
-}
-
 .inside-div {
   background: gray;
 }
--- src/views/Voting/VotingOver.vue
+++ src/views/Voting/VotingOver.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-row>
-      <v-col>
+      <v-col class="inside-div">
         <h2 class="ballot-header">
           Voting is over!
         </h2>
@@ -14,16 +14,21 @@
 </template>
 
 <script>
+// TODO: this and VoteWoah should be a single component with props
 export default {
-  name: 'VotingWoah',
+  name: "VotingWoah"
 };
 </script>
 
 <style lang="scss" scoped>
+.inside-div {
+  background: gray;
+}
+
 h2 {
   text-align: center;
   color: #0d1d41;
-  font-family: 'Barlow', sans-serif;
+  font-family: "Barlow", sans-serif;
   font-weight: bold;
   margin-bottom: 12px;
 }
@@ -31,6 +36,6 @@ h2 {
 p {
   text-align: center;
   color: #0d1d41;
-  font-family: 'Barlow', sans-serif;
+  font-family: "Barlow", sans-serif;
 }
 </style>
